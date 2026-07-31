c7c2e14b80ad45263735526e7a7612dccea61329
özgür kavak <ozgurkvk15@gmail.com>
2026-07-31 17:00:55 +0300

    Doktor.py Güncellendi

--- models/doktor.py
+++ models/doktor.py
@@ -1,21 +1,34 @@
 from models.kisi import Kisi
 
-class Doktor (Kisi):
-    def __init__(self, ad, soyad, yas, tc_kimlik_no, telefon,doktor_id,brans,uzmanlik,poliklinik,musaitlik):
-        super().__init__(ad,soyad,yas,tc_kimlik_no,telefon)
-        self.doktor_id=doktor_id
-        self.brans=brans
-        self.uzmanlık=uzmanlik
-        self.poliklinik=poliklinik
-        self.musaitlik=musaitlik
+
+class Doktor(Kisi):
+    def __init__(
+        self,
+        ad,
+        soyad,
+        yas,
+        tc_kimlik_no,
+        telefon,
+        doktor_id,
+        brans,
+        uzmanlik,
+        poliklinik,
+        musaitlik
+    ):
+        super().__init__(ad, soyad, yas, tc_kimlik_no, telefon)
+
+        self.doktor_id = doktor_id
+        self.brans = brans
+        self.uzmanlik = uzmanlik
+        self.poliklinik = poliklinik
+        self.musaitlik = musaitlik
 
     def __str__(self):
-              return (
-                f"{super().__str__()}\n"
-                f"Doktor ID: {self.doktor_id}\n"
-                f"Branş: {self.brans}\n"
-                f"Uzmanlık: {self.uzmanlık}\n"
-                f"Polikilinik: {self.poliklinik}\n"
-                f"Müsaitlik: {self.musaitlik}"
-            )     
-        
+        return (
+            f"{super().__str__()}\n"
+            f"Doktor ID: {self.doktor_id}\n"
+            f"Branş: {self.brans}\n"
+            f"Uzmanlık: {self.uzmanlik}\n"
+            f"Poliklinik: {self.poliklinik}\n"
+            f"Müsaitlik: {self.musaitlik}"
+        )
